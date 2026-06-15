7764491d461a7ad3cc8186f47f7a52a8d24b0d0e
Neal <ivnealrau@icloud.com>
2026-06-15 17:48:15 -0400

    Fixed Lock Functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import FigmentLock from './figment-lock/FigmentLock'
 
-   ReactDOM.createRoot(document.getElementById('root')).render(
-     <React.StrictMode>
-       <FigmentLock siteId="977841b1-f003-44ca-b3c3-d67b745a49b1">
-         <App />
-       </FigmentLock>
-     </React.StrictMode>
-   )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <FigmentLock siteId="977841b1-f003-44ca-b3c3-d67b745a49b1">
+      <App />
+    </FigmentLock>
+  </StrictMode>,
+)
