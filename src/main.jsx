5226a2f97f7b47a3502688e9b712b8b9fed16f8f
Neal <ivnealrau@icloud.com>
2026-06-16 20:39:46 -0400

    Remove Lock 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import FigmentLock from './figment-lock/FigmentLock'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
